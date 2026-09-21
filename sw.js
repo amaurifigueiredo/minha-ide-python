7e32cb9fc4977faab94226be742fd3712e92c159
amaurifigueiredo <amaurifigueiredo@gmail.com>
2026-09-21 11:54:45 -0300

    Upgrade cache version and implement service worker events
    Updated cache version and added activation event handling.

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'pyide-v1';
+const CACHE_NAME = 'pyide-v2';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
